3988c198d7243f184422d3efc5d4530c3734d451
Terrence Neumann <terrenceneumann2016@u.northwestern.edu>
2017-09-20 10:18:44 -0500

    Include dev.ratio for fitted series

--- R/ElasticSynth.R
+++ R/ElasticSynth.R
@@ -16,10 +16,11 @@
 #' @param penaltyMat glmnet param - (optional) a n-1 x n matrix of additional penalties placed upon glmnet for selecting certain units. Useful in the case of multiple treated units. Each column of the matrix represents the penalties to be placed on the control units. The treated unit is excluded from the column. See glmnet param penalty.factor for more details. Default is no additional penalties on any unit.
 #' @param max_number_units glmnet param - The maximum number of units glmnet will select. Useful for forcing parsimonious models. Default allows all units to receive weight.
 #' @param lower_limit_weights The lower limit value of weight that can be placed on any unit. Default is zero (non-negative constraint). Change if desired, but be wary of overfitting.
-#' @return list containing output weights for treated unit (w_final), the actual outcome values (Y_true), the fitted outcome values (Y_elast), optimal value of lambda (lambda_opt), optimal value of alpha, (alpha_opt), a dataframe of the results of placebo test (placebo_frame), a plot of the path of the treated vs. actual unit (path.plot), and a plot of the placebo test results for the treated unit. 
+#' @param cv The number of periods of pre-data to include in cross validation step. Helpful to include more if you don't have much post-period data
+#' @return list containing output weights for treated unit (w_final), the actual outcome values (Y_true), the fitted outcome values (Y_elast), optimal value of lambda (lambda_opt), optimal value of alpha, (alpha_opt), a dataframe of the results of placebo test (placebo_frame), a plot of the path of the treated vs. actual unit (path.plot), the deviation ratio of the fitted series (dev.ratio), and a plot of the placebo test results for the treated unit. 
 #' @export
 
-ElasticSynth <- function(PredictorMatrix, 
+ElasticSynth = function(PredictorMatrix, 
                          OutcomeMatrix, 
                          treated, 
                          pre, 
@@ -43,7 +44,7 @@ ElasticSynth <- function(PredictorMatrix,
   suppressMessages(library(lubridate))
   
   #### functions
-  penaltyCleanup <- function(penalty, treated, i) {
+  penaltyCleanup = function(penalty, treated, i) {
     if (i <= treated & i > 1) {
       penaltyTreated = penalty[treated - 1]
       penaltyMinusTreated = penalty[-(treated - 1)]
@@ -59,77 +60,77 @@ ElasticSynth <- function(PredictorMatrix,
   
   #### necessary data structure
   # Predictors for units minus treated
-  X0 <- as.matrix(PredictorMatrix[,-treated])
+  X0 = as.matrix(PredictorMatrix[,-treated])
   # Predictors for treated
-  X1 <- as.matrix(PredictorMatrix[,treated, drop = F])
+  X1 = as.matrix(PredictorMatrix[,treated, drop = F])
   # Combined
-  X  <- as.matrix(cbind(X1, X0))
+  X  = as.matrix(cbind(X1, X0))
   # Time Periods for units minus treated
-  Y0 <- as.matrix(OutcomeMatrix[,-treated])
+  Y0 = as.matrix(OutcomeMatrix[,-treated])
   # Time Periods for treated
-  Y1 <- as.matrix(OutcomeMatrix[,treated, drop = F])
+  Y1 = as.matrix(OutcomeMatrix[,treated, drop = F])
   # Combined
-  Y  <- as.matrix(cbind(Y1, Y0))
+  Y  = as.matrix(cbind(Y1, Y0))
   # Pre period units minus treated
-  Z0 <- as.matrix(Y0[pre,])
+  Z0 = as.matrix(Y0[pre,])
   # Pre period for treated
-  Z1 <- as.matrix(Y1[pre,1, drop = F])
+  Z1 = as.matrix(Y1[pre,1, drop = F])
   # Combined
-  Z  <- as.matrix(cbind(Z1, Z0))
+  Z  = as.matrix(cbind(Z1, Z0))
   
   ##### Params
   
   # Number of Predictors
-  K  <- dim(X)[1]
+  K  = dim(X)[1]
   # Number of units
-  N  <- dim(Y)[2]
+  N  = dim(Y)[2]
   # Total time periods
-  Time <- dim(Y)[1]
+  Time = dim(Y)[1]
   # Pre period
-  T0   <- dim(Z)[1]
+  T0   = dim(Z)[1]
   # Post Period
-  T1   <- Time - T0
+  T1   = Time - T0
   
-  div  <- as.matrix(apply(X, 1, sd))
-  X    <- X / div[,rep(1, N)]
+  div  = as.matrix(apply(X, 1, sd))
+  X    = X / div[,rep(1, N)]
   
   ## Month frame for plotting
-  start_month <- as.Date(start_month)
-  end_month <- as.Date(end_month)
-  month_seq  <- seq(start_month, end_month, by = time_unit)
-  month_join <- data.frame(time = 1:max(post), month = month_seq)
+  start_month = as.Date(start_month)
+  end_month = as.Date(end_month)
+  month_seq  = seq(start_month, end_month, by = time_unit)
+  month_join = data.frame(time = 1:max(post), month = month_seq)
   
   ## Find the optimal elast
   # Iterate over i
-  nlambda     <- length(lambda_grid)
-  na          <- length(a_grid)
-  err_alpha   <- matrix(0, nrow = na, ncol = 1)
-  lambda_opt_alpha <- matrix(0, nrow = na, ncol = 1)
+  nlambda     = length(lambda_grid)
+  na          = length(a_grid)
+  err_alpha   = matrix(0, nrow = na, ncol = 1)
+  lambda_opt_alpha = matrix(0, nrow = na, ncol = 1)
   
   cat('*** Searching for Optimal Lambda & Alpha ***\n')
-  penaltyMat  <- cbind(penaltyMat[,treated], penaltyMat[,-treated])
+  penaltyMat  = cbind(penaltyMat[,treated], penaltyMat[,-treated])
   
   for (j in 1:na) {
-    a         <- a_grid[j]
+    a         = a_grid[j]
     cat('a =', toString(a), '\n')
-    err       <- matrix(0, nrow = N, ncol = nlambda)
+    err       = matrix(0, nrow = N, ncol = nlambda)
     for (i in 1:N) {
-      Y1      <- as.matrix(Y[,i, drop = F])
-      Y0      <- as.matrix(Y[,-c(i)])
-      Z1      <- as.matrix(Z[,i, drop = F])
-      Z0      <- as.matrix(Z[,-c(i)])
-      X1      <- as.matrix(X[,i, drop = F])
-      X0      <- as.matrix(X[,-c(i)])
-      Z1_tr   <- Z1
-      Z0_tr   <- Z0
-      Z1_te   <- as.matrix(Y1[-(1:(T0-cv)),])
-      Z0_te   <- as.matrix(Y0[-(1:(T0-cv)),])
+      Y1      = as.matrix(Y[,i, drop = F])
+      Y0      = as.matrix(Y[,-c(i)])
+      Z1      = as.matrix(Z[,i, drop = F])
+      Z0      = as.matrix(Z[,-c(i)])
+      X1      = as.matrix(X[,i, drop = F])
+      X0      = as.matrix(X[,-c(i)])
+      Z1_tr   = Z1
+      Z0_tr   = Z0
+      Z1_te   = as.matrix(Y1[-(1:(T0-cv)),])
+      Z0_te   = as.matrix(Y0[-(1:(T0-cv)),])
 
       # CV - Find optimal lambda and alpha across all units
-      V1      <- rbind(scale(Z1_tr, scale = FALSE), scale(X1, scale = FALSE))
-      V0      <- rbind(scale(Z0_tr, scale = FALSE), scale(X0, scale = FALSE))
-      penalty <- penaltyCleanup(penaltyMat[,i, drop = F], treated, i)
-      fit     <- glmnet(x = V0, y = V1,
+      V1      = rbind(scale(Z1_tr, scale = FALSE), scale(X1, scale = FALSE))
+      V0      = rbind(scale(Z0_tr, scale = FALSE), scale(X0, scale = FALSE))
+      penalty = penaltyCleanup(penaltyMat[,i, drop = F], treated, i)
+      fit     = glmnet(x = V0, y = V1,
                         alpha = a,
                         lambda = lambda_grid,
                         standardize = FALSE,
@@ -137,44 +138,44 @@ ElasticSynth <- function(PredictorMatrix,
                         lower.limits = lower_limit_weights,
                         pmax = max_number_units,
                         penalty.factor = penalty)
-      w       <- as.matrix(coef(fit, s = lambda_grid))[-1,]
-      int     <- t(as.matrix(apply(Z1_tr[,rep(1, nlambda)] - Z0_tr %*% w, 2, mean)))
-      e       <- Z1_te[,rep(1, nlambda)] - int[rep(1, T1 + cv),] - Z0_te %*% w
-      err[i,] <- colMeans(e^2, na.rm = TRUE)
+      w       = as.matrix(coef(fit, s = lambda_grid))[-1,]
+      int     = t(as.matrix(apply(Z1_tr[,rep(1, nlambda)] - Z0_tr %*% w, 2, mean)))
+      e       = Z1_te[,rep(1, nlambda)] - int[rep(1, T1 + cv),] - Z0_te %*% w
+      err[i,] = colMeans(e^2, na.rm = TRUE)
     }
     # Optimal lambda
-    err       <- apply(err, 2, mean)
-    ind_opt   <- which.min(err)
-    err_alpha[j] <- err[ind_opt]
-    lambda_opt_alpha[j] <- lambda_grid[ind_opt]
+    err       = apply(err, 2, mean)
+    ind_opt   = which.min(err)
+    err_alpha[j] = err[ind_opt]
+    lambda_opt_alpha[j] = lambda_grid[ind_opt]
   }
   # Optimal a
-  ind_opt     <- which.min(err_alpha)
-  a_opt       <- a_grid[ind_opt]
-  lambda_opt  <- lambda_opt_alpha[ind_opt]
+  ind_opt     = which.min(err_alpha)
+  a_opt       = a_grid[ind_opt]
+  lambda_opt  = lambda_opt_alpha[ind_opt]
   
   
 
-  int_elast   <- matrix(0, nrow = 1, ncol = 1)
-  Y_elast     <- matrix(0, nrow = Time, ncol = 1)
-  Y_true      <- matrix(0, nrow = Time, ncol = 1)
-  old_frame   <- data.frame()
+  int_elast   = matrix(0, nrow = 1, ncol = 1)
+  Y_elast     = matrix(0, nrow = Time, ncol = 1)
+  Y_true      = matrix(0, nrow = Time, ncol = 1)
+  old_frame   = data.frame()
     for (i in 1:N) {
       if (i != 1) {
-        old_frame <- new_frame
+        old_frame = new_frame
       }
-      Y1 <- as.matrix(Y[,i,drop = F])
-      Y0 <- as.matrix(Y[,-i])
-      Z1 <- as.matrix(Z[,i, drop = F])
-      Z0 <- as.matrix(Z[,-i])
-      X1 <- as.matrix(X[,i, drop = F])
-      X0 <- as.matrix(X[,-i])
-      V1 <- rbind(scale(Z1, scale = FALSE), scale(X1, scale = FALSE))
-      V0 <- rbind(scale(Z0, scale = FALSE), scale(X0, scale = FALSE))
+      Y1 = as.matrix(Y[,i,drop = F])
+      Y0 = as.matrix(Y[,-i])
+      Z1 = as.matrix(Z[,i, drop = F])
+      Z0 = as.matrix(Z[,-i])
+      X1 = as.matrix(X[,i, drop = F])
+      X0 = as.matrix(X[,-i])
+      V1 = rbind(scale(Z1, scale = FALSE), scale(X1, scale = FALSE))
+      V0 = rbind(scale(Z0, scale = FALSE), scale(X0, scale = FALSE))
       penalty = penaltyCleanup(penaltyMat[,i, drop = F], treated, i)
       
       # Fit elast
-      fit_final   <- glmnet(x = V0, y = V1,
+      fit_final   = glmnet(x = V0, y = V1,
                             alpha = a_opt,
                             lambda = lambda_grid,
                             standardize = FALSE,
@@ -182,24 +183,25 @@ ElasticSynth <- function(PredictorMatrix,
                             pmax = max_number_units,
                             lower.limits = lower_limit_weights,
                             penalty.factor = penalty)
-      w           <- as.matrix(coef(fit_final, s = lambda_opt))[-1,]
+      w           = as.matrix(coef(fit_final, s = lambda_opt))[-1,]
       if (i == 1) {
-        w_final     <- w
-        int_elast   <- as.matrix(apply(Z1 - Z0 %*% w_final, 2, mean))
-        Y_elast     <- int_elast[rep(1, Time),] + Y0 %*% w_final
-        Y_elast_final <- Y_elast
-        Y_true      <- Y1[c(pre,post)]
-        Y_true_final <- Y_true
-        gaps        <- Y_true[c(pre,post)] - Y_elast[c(pre,post)]
-        rmse        <- sqrt(sum(abs(gaps[pre]))/length(pre))
+        dev.ratio   = fit_final$dev.ratio
+        w_final     = w
+        int_elast   = as.matrix(apply(Z1 - Z0 %*% w_final, 2, mean))
+        Y_elast     = int_elast[rep(1, Time),] + Y0 %*% w_final
+        Y_elast_final = Y_elast
+        Y_true      = Y1[c(pre,post)]
+        Y_true_final = Y_true
+        gaps        = Y_true[c(pre,post)] - Y_elast[c(pre,post)]
+        rmse        = sqrt(sum(abs(gaps[pre]))/length(pre))
         
         
-        plotFrame   <- data.frame(time = c(pre,post), Y_true = Y_true, Y_elast = Y_elast, gaps = gaps)
-        plotFrame   <- merge(plotFrame, month_join, by = 'time', all.x = TRUE)
-        min_post_month <- plotFrame$month[plotFrame$time == min(post)]
-        max_post_month <- plotFrame$month[plotFrame$time == max(post)]
+        plotFrame   = data.frame(time = c(pre,post), Y_true = Y_true, Y_elast = Y_elast, gaps = gaps)
+        plotFrame   = merge(plotFrame, month_join, by = 'time', all.x = TRUE)
+        min_post_month = plotFrame$month[plotFrame$time == min(post)]
+        max_post_month = plotFrame$month[plotFrame$time == max(post)]
         # 
-        path.plot   <- ggplot(plotFrame) +
+        path.plot   = ggplot(plotFrame) +
           geom_line(aes(x = as.Date(month), y = Y_true), colour = 'black', size = 2) +
           geom_line(aes(x = as.Date(month), y = Y_elast), colour = 'indianred3', linetype = 'longdash', size = 2) +
           geom_rect(aes(xmin = min_post_month, xmax = max_post_month, ymin = -Inf, ymax = Inf), color="olivedrab2",
@@ -218,23 +220,23 @@ ElasticSynth <- function(PredictorMatrix,
         
       }
       
-      int_elast   <- as.matrix(apply(Z1 - Z0 %*% w, 2, mean))
-      Y_elast     <- int_elast[rep(1, Time),] + Y0 %*% w
-      Y_true      <- Y1
-      gaps        <- Y_true[c(pre,post)] - Y_elast[c(pre,post)]
-      rmse        <- sqrt(sum(abs(gaps[pre]))/length(pre))
-      new_frame   <- data.frame(gaps = unlist(gaps), time = c(pre,post), unit_type = ifelse(i == 1, 'Treated Unit', 'Control Unit Distribution'), unit = colnames(Y)[i])
-      new_frame   <- rbind(old_frame, new_frame)
+      int_elast   = as.matrix(apply(Z1 - Z0 %*% w, 2, mean))
+      Y_elast     = int_elast[rep(1, Time),] + Y0 %*% w
+      Y_true      = Y1
+      gaps        = Y_true[c(pre,post)] - Y_elast[c(pre,post)]
+      rmse        = sqrt(sum(abs(gaps[pre]))/length(pre))
+      new_frame   = data.frame(gaps = unlist(gaps), time = c(pre,post), unit_type = ifelse(i == 1, 'Treated Unit', 'Control Unit Distribution'), unit = colnames(Y)[i])
+      new_frame   = rbind(old_frame, new_frame)
       
     }
-    new_frame     <- merge(new_frame, month_join, by = 'time', all.x = TRUE)
-    min_post_month <- unique(new_frame$month[new_frame$time == min(post)])
-    max_post_month <- unique(new_frame$month[new_frame$time == max(post)])
-    far_x_axis_month <- max_post_month %m+% months(2)
-    near_x_axis_month <- max_post_month %m-% months(3*10)
+    new_frame     = merge(new_frame, month_join, by = 'time', all.x = TRUE)
+    min_post_month = unique(new_frame$month[new_frame$time == min(post)])
+    max_post_month = unique(new_frame$month[new_frame$time == max(post)])
+    far_x_axis_month = max_post_month %m+% months(2)
+    near_x_axis_month = max_post_month %m-% months(3*10)
     
     
-    placebo <- ggplot() +
+    placebo = ggplot() +
       geom_line(data = subset(new_frame, unit_type == 'Control Unit Distribution'), aes(x = month, y = gaps, group = unit),colour = 'grey', size = 1) +
       geom_line(data = subset(new_frame, unit_type == 'Treated Unit'), aes(x = month, y = gaps), size = 2, colour = 'black') +
       geom_hline(aes(yintercept = 0)) +
@@ -263,7 +265,8 @@ ElasticSynth <- function(PredictorMatrix,
                 alpha_opt = a_opt,
                 placebo_frame = new_frame,
                 placebo = placebo,
-                path.plot = path.plot))
+                path.plot = path.plot,
+                dev.ratio = dev.ratio))
     
     
 }
