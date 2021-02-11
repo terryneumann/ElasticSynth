c1fedf7a93b95b1b9debbba3e5c88c1ccebdf0f9
Terrence Neumann <terrenceneumann2016@u.northwestern.edu>
2021-02-11 10:49:45 -0600

    Update demo.R

--- examples/demo.R
+++ examples/demo.R
@@ -2,8 +2,8 @@
 crime_data <- generate_public_crime_data()
 treated_units <- crime_data$treated_units
 donor_units <- crime_data$donor_units
-treated_units <- read.csv('~/treated_units.csv')
-donor_units <- read.csv('~/donor_units.csv')
+#treated_units <- read.csv('~/treated_units.csv')
+#donor_units <- read.csv('~/donor_units.csv')
 head(treated_units, n = 30)
 head(donor_units, n = 30)
 
