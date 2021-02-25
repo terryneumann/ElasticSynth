70e17070658eb41b08d8e3a87e08497676c07bfe
Terrence Neumann <tdneumann@uchicago.edu>
2021-02-24 19:55:57 -0600

    get() 2

--- R/ElasticSynth.R
+++ R/ElasticSynth.R
@@ -186,11 +186,11 @@ ElasticSynth <- R6::R6Class(
         pre = self$pre_list[[m]]
         post = self$post_list[[m]]
         
-        treated_wide_measure = self$long_to_wide(self$treated_units[get(self$measure_col) == self$measure_vars[m], ],
+        treated_wide_measure = self$long_to_wide(self$treated_units[get(as.name(self$measure_col)) == self$measure_vars[m], ],
                                             self$time_col,
                                             self$unit_col,
                                             self$value_col)
-        donor_wide_measure = self$long_to_wide(self$donor_units[get(self$measure_col) == self$measure_vars[m], ],
+        donor_wide_measure = self$long_to_wide(self$donor_units[get(as.name(self$measure_col)) == self$measure_vars[m], ],
                                           self$time_col,
                                           self$unit_col,
                                           self$value_col)
